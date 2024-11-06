eeba5e3ea2000c4bcbe9aa309819433e25401a47
KellyCNunes <kelly.crisnunes@hotmail.com>
2024-11-05 21:38:45 -0300

    05/11

---
+++ src/Atividade04.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Atividade04() {
+    return (
+      <>
+      <h2> Atividade 04</h2>
+      <Contador/>
+
+      <Link to ="/"> Voltar</Link>
+      </>
+    );
+  }
+  
+  export default Atividade04;
